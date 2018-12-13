960e1f4b40d3bcdc9aa3a87d056993cba84b9f7b
wuqihan <wuqihan@able-elec.com>
2018-12-13 11:44:12 +0800

    init

---
+++ DDDownload/DDDownload/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DDDownload
+//
+//  Created by wuqh on 2018/12/13.
+//  Copyright © 2018 wuqh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
